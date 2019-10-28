e65fedd61835f0f92ffe94c77dd810d4b819465f
kimbe <kimbe@KimberlyLaptop>
2019-10-27 21:55:06 -0500

    end of homework - pushed ArrayGreaterThanN, InvalidTestScore, TestData, TestScores, and TestScoresDemonstrate

---
+++ src/main/java/TestScoresDemonstrate.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author kimbe
+ */
+public class TestScoresDemonstrate {
+    public static void main(String[] args) {
+        
+        //I got lost and cannot figure out what goes in the main method.
+        // Should this be in the TestScores class?
+        
+    }
+}
+    
